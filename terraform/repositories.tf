352248aaf6e21f7aef917a9529bd8309f462570f
Thomas Boerger <thomas@webhippie.de>
2018-07-15 22:21:48 +0200

    Integrate github management (#1)
    * Integrate github management
    * Fix drone conditions

---
+++ terraform/repositories.tf
@@ -0,0 +1,179 @@
+resource "github_repository" "gopad-android" {
+  name               = "gopad-android"
+  description        = "Gopad: Android client"
+  homepage_url       = "https://gopad.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gopad-api" {
+  name               = "gopad-api"
+  description        = "Gopad: API server"
+  homepage_url       = "https://gopad.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gopad-cli" {
+  name               = "gopad-cli"
+  description        = "Gopad: CLI client"
+  homepage_url       = "https://gopad.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gopad-desktop" {
+  name               = "gopad-desktop"
+  description        = "Gopad: Desktop client"
+  homepage_url       = "https://gopad.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gopad-docs" {
+  name               = "gopad-docs"
+  description        = "Gopad: Documentation"
+  homepage_url       = "https://gopad.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gopad-go" {
+  name               = "gopad-go"
+  description        = "Gopad: SDK for Go"
+  homepage_url       = "https://gopad.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gopad-infra" {
+  name               = "gopad-infra"
+  description        = "Gopad: Infrastructure"
+  homepage_url       = "https://gopad.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gopad-ios" {
+  name               = "gopad-ios"
+  description        = "Gopad: iOS client"
+  homepage_url       = "https://gopad.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gopad-js" {
+  name               = "gopad-js"
+  description        = "Gopad: SDK for Javascript"
+  homepage_url       = "https://gopad.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gopad-php" {
+  name               = "gopad-php"
+  description        = "Gopad: SDK for PHP"
+  homepage_url       = "https://gopad.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gopad-python" {
+  name               = "gopad-python"
+  description        = "Gopad: SDK for Python"
+  homepage_url       = "https://gopad.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gopad-ruby" {
+  name               = "gopad-ruby"
+  description        = "Gopad: SDK for Ruby"
+  homepage_url       = "https://gopad.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gopad-scripts" {
+  name               = "gopad-scripts"
+  description        = "Gopad: Scripting"
+  homepage_url       = "https://gopad.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gopad-ui" {
+  name               = "gopad-ui"
+  description        = "Gopad: Web UI"
+  homepage_url       = "https://gopad.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "homebrew-gopad" {
+  name               = "homebrew-gopad"
+  description        = "Gopad: Homebrew"
+  homepage_url       = "https://gopad.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,3 +11,81 @@ variable "cloudflare_domain" {
   type    = "string"
   default = "gopad.tech"
 }
+
+variable "github_token" {
+  type = "string"
+}
+
+variable "github_org" {
+  type    = "string"
+  default = "gopad"
+}
+
+variable "github_members" {
+  type = "map"
+
+  default = {
+    gopadz   = "admin"
+    tboerger = "admin"
+  }
+}
+
+variable "bot_members" {
+  type = "map"
+
+  default = {
+    gopadz = "maintainer"
+  }
+}
+
+variable "bot_repos" {
+  type = "map"
+
+  default = {
+    gopad-android  = "admin"
+    gopad-api      = "admin"
+    gopad-cli      = "admin"
+    gopad-desktop  = "admin"
+    gopad-docs     = "admin"
+    gopad-go       = "admin"
+    gopad-infra    = "admin"
+    gopad-ios      = "admin"
+    gopad-js       = "admin"
+    gopad-php      = "admin"
+    gopad-python   = "admin"
+    gopad-ruby     = "admin"
+    gopad-scripts  = "admin"
+    gopad-ui       = "admin"
+    homebrew-gopad = "admin"
+  }
+}
+
+variable "maintainer_members" {
+  type = "map"
+
+  default = {
+    tboerger = "maintainer"
+  }
+}
+
+variable "maintainer_repos" {
+  type = "map"
+
+  default = {
+    gopad-android  = "push"
+    gopad-api      = "push"
+    gopad-cli      = "push"
+    gopad-desktop  = "push"
+    gopad-docs     = "push"
+    gopad-go       = "push"
+    gopad-infra    = "push"
+    gopad-ios      = "push"
+    gopad-js       = "push"
+    gopad-php      = "push"
+    gopad-python   = "push"
+    gopad-ruby     = "push"
+    gopad-scripts  = "push"
+    gopad-ui       = "push"
+    homebrew-gopad = "push"
+  }
+}
